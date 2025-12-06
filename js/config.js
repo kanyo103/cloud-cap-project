c4f35dc78680ec25d9554bdbf9c4f69e3be1c454
Karabo Lelaka <karabolesole03@gmail.com>
2025-12-06 10:07:17 +0200

    Update configuration for Cognito and API

--- js/config.js
+++ js/config.js
@@ -1,10 +1,26 @@
-window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
+// window._config = {
+//     cognito: {
+//         userPoolId: '', // e.g. us-east-2_uXboG5pAb
+//         userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+//         region: '' // e.g. us-east-2
+//     },
+//     api: {
+//         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+//     }
+// };
+
+
+// --- CONFIGURATION START ---
+        window._config = {
+            cognito: {
+                userPoolId: 'us-east-1_yIH0E3fH6', 
+                userPoolClientId: '21cmgmfm45ppuc90elgd5mmhj9',
+                region: 'us-east-1',
+                loginDomain: 'https://21cmgmfm45ppuc90elgd5mmhj9.auth.us-east-1.amazoncognito.com'
+            },
+            api: {
+                // UPDATE THIS LINE BELOW:
+                invokeUrl: 'https://ujddx3cixh.execute-api.us-east-1.amazonaws.com/Prod'
+            }
+        };
+        // --- CONFIGURATION END ---
